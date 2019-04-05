0d56375c5f1fb65e9e22a7b27818ee0828161dfd
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-05 16:29:28 +0200

    Route import

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 // Route imports
 const testRoute = require('./routes/test');
+const authRoute = require('./routes/auth');
+const profileRoute = require('./routes/profile');
 
 // Dotenv
 require('dotenv').config();
@@ -14,5 +16,8 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api/test', testRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server running on http://localhost:${process.env.SERVER_PORT}/`));
