2d147839a3c11088a05258b42f49c9dcf2872ba1
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-05 15:13:30 +0200

    Removed Typescript, going for vanilla JS

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.listen(process.env.SERVER_PORT, () => console.log(`Server running on http://localhost:${process.env.SERVER_PORT}/`));
