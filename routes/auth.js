922f717ce9d8985db89a1cdd3a1fa22506e0be6c
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-06 15:39:25 +0200

    Login

--- routes/auth.js
+++ routes/auth.js
@@ -10,30 +10,26 @@ router.post('/register', (req, res) => {
     //TODO: => Register a new user
     // Check if the provided input is valid
     const inputErrors = inputValidation.register(req.body);
+    // If the function returns false, in regards of no errors being returned, proceed
     if(!inputErrors) {
-        // If the function returns false, in regards of no errors being returned, proceed
+        const { email, password } = req.body;
         // Check if user exists via email
-        User.findOne({ where: { email: req.body.email } })
+        User.findOne({ where: { email } })
         .then(user => {
             if(!user) {
                 // If there's no user, register a new one
-                const { email, password } = req.body;
                 // Hash the password
                 bcrypt.hash(password, 10, (err, hash) => {
-                    if(err) {
+                    if (err) {
                         console.log(err);
                     } else {
-                        const { email, password } = req.body;
-                // Hash the password
-                bcrypt.hash(password, 10, (err, hash) => {
-                    if(err) {
-                        console.log(err);
-                    } else {
-                        User.create({ email, password: hash })
+                        // Save the user in the database
+                        User.create({
+                                email,
+                                password: hash
+                            })
                             .then(user => res.status(200).json(user))
                             .catch(err => res.status(500).json(err))
-                    }
-                })
                     }
                 })
             } else {
@@ -53,6 +49,41 @@ router.post('/register', (req, res) => {
 // DESC:    =>  Log in 
 router.post('/login', (req, res) => {
     //TODO: => Log in
+    // Check for input errors
+    const inputErrors = inputValidation.login(req.body);
+    // If no errors, proceed
+    if(!inputErrors) {
+        const { email, password } = req.body;
+        // Check if user exists within the database
+        User.findOne({ where: { email } })
+            .then(user => {
+                // Send error message if there's no user
+                if(!user) {
+                    res.status(404).json({ error: 'User not found.' })
+                } else {
+                    // Compare provided password with the hash
+                    bcrypt.compare(password, user.password, (err, match) => {
+                        if(err) {
+                            // Log the error if there's an error
+                            console.error(err);
+                        } else {
+                            // If no match, send an error
+                            if(!match) {
+                                res.status(400).json({ error: 'Invalid password.' });
+                            } else {
+                                //TODO: => Send an authorization token
+                                res.status(200).json({ loggedIn: match });
+                            }
+                        }
+                    })
+                }
+            })
+    } else {
+        // Return input errors
+        res.status(400).json(inputErrors);
+    }
+    // Check if user exists
+
 });
 
 // ROUTE:   =>  /auth/current 
