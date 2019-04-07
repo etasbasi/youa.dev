c0cdb92a03610a5762122024620f3a96bd98950e
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-07 13:33:37 +0200

    Added more data to user model

--- db/models/User.js
+++ db/models/User.js
@@ -2,11 +2,17 @@ const Sequelize = require('sequelize');
 const Database = require('../Database');
 
 const User = Database.define('User', {
+    type: {
+        type: Sequelize.STRING
+    },
     email: {
         type: Sequelize.STRING
     },
     password: {
         type: Sequelize.STRING
+    },
+    verified: {
+        type: Sequelize.BOOLEAN
     }
 })
 
