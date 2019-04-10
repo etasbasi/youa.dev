07ed2ad74db0d8a23637692c3200650da791bb25
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-11 01:14:41 +0200

    Initiliazed client app, added CORS

---
+++ client/src/Landing.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
+import "./scss/Base.scss";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <h1>youa.dev</h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
--- server.js
+++ server.js
@@ -1,46 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const config = require('./config/config');
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./config/config");
 
 // App
 const app = express();
 
 // Route imports
-const testRoute = require('./routes/test');
-const authRoute = require('./routes/auth');
-const profileRoute = require('./routes/profile');
-const postsRoute = require('./routes/posts');
-const supportRoute = require('./routes/support');
-const adminRoute = require('./routes/admin');
+const testRoute = require("./routes/test");
+const authRoute = require("./routes/auth");
+const profileRoute = require("./routes/profile");
+const postsRoute = require("./routes/posts");
+const supportRoute = require("./routes/support");
+const adminRoute = require("./routes/admin");
+
+// FIXME: => CORS - Remove before deployment
+const cors = require("cors");
+app.use(cors());
 
 // Middleware
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
 
 // Passport
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Routes
-app.use('/api/test', testRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/profile', profileRoute);
-app.use('/api/posts', postsRoute);
-app.use('/api/support', supportRoute);
-app.use('/api/admin', adminRoute);
-
+app.use("/api/test", testRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/support", supportRoute);
+app.use("/api/admin", adminRoute);
 
 // Database
-const Database = require('./db/Database');
+const Database = require("./db/Database");
 Database.authenticate()
-    .then(() => console.log('Database connection established.'))
-    .catch(err => console.error('Connection refused. Error: ', err));
+  .then(() => console.log("Database connection established."))
+  .catch(err => console.error("Connection refused. Error: ", err));
 
-//TODO: => Remove this function - ADMIN ONLY
+// FIXME: => Admin only - Remove before deployment
 Database.sync();
 
 // Server Init
-app.listen(config.SERVER_PORT, () => console.log(`Server running on http://localhost:${config.SERVER_PORT}/`));
+app.listen(config.SERVER_PORT, () =>
+  console.log(`Server running on http://localhost:${config.SERVER_PORT}/`)
+);
