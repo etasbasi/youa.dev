04ddf6bd57878eb3f9fb0e0b29dd19c05b05aecd
Dennis Onder <webdev.dennisonder@gmail.com>
2019-04-13 03:14:30 +0200

    Landing page tweaks

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <h1>youa.dev</h1>
+        <h1 className="landing_title">youa.dev</h1>
+        <p className="landing_motto">
+          A networking hub for developers. From developers.
+        </p>
+        <div className="landing_get-started">
+          <Link to="/register" className="landing_get-started--redirect">
+            Click here to get started.
+          </Link>
+        </div>
       </div>
     );
   }
